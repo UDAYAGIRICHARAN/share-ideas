fbc32e69ca6ab585d0faba6935ddbab0ca039cb5
UDAYAGIRI CHARAN KUMAR <67089878+UDAYAGIRICHARAN@users.noreply.github.com>
2021-07-24 22:48:38 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from config import *
+from app import app
+import helpers
+
+app.jinja_env.globals.update(helpers=helpers)
+
+db.create_all()
+
+if __name__ == '__main__':
+    app.debug = True
+    app.jinja_env.auto_reload = True
+    app.run('127.0.0.1',5000)
