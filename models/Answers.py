1232c0e9f429039ed8f625b1efab5afc54b9b584
UDAYAGIRI CHARAN KUMAR <67089878+UDAYAGIRICHARAN@users.noreply.github.com>
2021-08-10 10:18:38 +0530

    Add files via upload

---
+++ models/Answers.py
@@ -0,0 +1,12 @@
+from config import db
+
+class Answer(db.Model):
+    __tablename__ = "answers"
+    a_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    q_id = db.Column(db.Integer, nullable=False)
+    body = db.Column(db.Text)
+    author_u_id = db.Column(db.Integer)
+    post_time = db.Column(db.DateTime)
+    upvotes = db.Column(db.Integer)
+    upvoters = db.Column(db.Text)
+    reports = db.Column(db.Integer)
